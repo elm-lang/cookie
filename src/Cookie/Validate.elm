5a4e8ab9af9f433ed76d75a2070ff00dc9b45711
Richard Feldman <richard.t.feldman@gmail.com>
2016-06-10 16:43:49 -0700

    Simplify Cookie.Validate and improve error messages.

--- src/Cookie/Validate.elm
+++ src/Cookie/Validate.elm
@@ -20,18 +20,16 @@ http://stackoverflow.com/questions/1969232/allowed-characters-in-cookies/1969339
 -}
 illegalValueChar : Regex
 illegalValueChar =
-    Regex.regex "(?:[^\\x00-\\x7F]|[;,\\s])"
+    Regex.regex "[^a-zA-Z0-9#!$%&'()*+-./:<=>?@\\[\\]^_`{|}~]"
 
 
-{-| Identifies characters that would make a cookie value illegal. Built for
+{-| Identifies characters that would make a cookie key illegal. Built for
 speed, not for error messages! Other regexes can be used to find specific errors
 in the event that this fails.
-
-This is the same as illegalValueChar, but with = characters banned as well.
 -}
 illegalKeyChar : Regex
 illegalKeyChar =
-    Regex.regex "(?:[^\\x00-\\x7F]|[;,=\\s])"
+    Regex.regex "[^a-zA-Z0-9!#$%&'*+-.^_`|~]"
 
 
 {-| I started out by reading http://stackoverflow.com/questions/1969232/allowed-characters-in-cookies/1969339#1969339
@@ -57,43 +55,66 @@ cross-browser, so we should not allow it.
 
 -@rtfeldman
 -}
-nonStandardAscii : Regex
-nonStandardAscii =
-    Regex.regex "[^\\x00-\\x7F]"
-
-
-allStandardAscii : Regex
-allStandardAscii =
-    Regex.regex ("^[\\x00-\\x7F]*$")
-
-
-detailedError : String -> String
-detailedError str =
+valueError : String -> String
+valueError str =
     let
         matches =
             str
-                |> Regex.find Regex.All nonStandardAscii
+                |> Regex.find Regex.All illegalValueChar
                 |> List.map .match
     in
         String.concat
-            [ "Cookie keys and values may only contain Standard ASCII characters. "
-            , "They also may not contain control characters. "
-            , "Several browsers mangle or silently refuse to send Unicode and "
-            , "Extended ASCII characters, so they are not supported.\n\n"
+            [ "For reliable cross-browser cookie setting, "
+            , "cookie values may only contain Standard ASCII letters, numbers, "
+            , "and the following special characters:\n\n"
+            , "!#$%&'()*+-./:<=>?@[]^_`{|}~"
             , "Using encodeURIComponent may help here!\n\n"
+            , "See http://stackoverflow.com/a/1969339 for further explanation.\n"
             , "Unsupported character sequences: "
             , toString matches
-            , "\n\nComplete Value: "
+            , "\n\nOriginal value: "
             , str
             ]
+
+
+keyError : String -> String
+keyError str =
+    let
+        matches =
+            str
+                |> Regex.find Regex.All illegalKeyChar
+                |> List.map .match
+    in
+        String.concat
+            [ "For reliable cross-browser cookie setting, "
+            , "cookie keys may only contain Standard ASCII letters, numbers, "
+            , "and the following special characters:\n\n"
+            , "!#$%&'*+-.^_`|~"
+            , "Using encodeURIComponent may help here!\n\n"
+            , "See http://stackoverflow.com/a/1969339 for further explanation.\n"
+            , "Unsupported character sequences: "
+            , toString matches
+            , "\n\nOriginal value: "
+            , str
+            ]
+
+
+emptyKeyError : String
+emptyKeyError =
+    String.join " "
+        [ "Cookies with empty keys are not supported."
+        , "Different browsers handle them differently,"
+        , "which can lead to nasty bugs on the server."
+        , "Provide a key now to avoid crazy problems later!"
+        ]
 
 
 key : String -> Maybe String
 key str =
     if String.isEmpty str then
-        Just "Cookies with empty keys are not supported. Different browsers handle them differently, which can lead to nasty bugs on the server. Provide a key now to avoid crazy problems later!"
+        Just emptyKeyError
     else if (Regex.contains illegalKeyChar str) then
-        Just (detailedError str)
+        Just (keyError str)
     else
         Nothing
 
@@ -101,6 +122,6 @@ key str =
 value : String -> Maybe String
 value str =
     if (Regex.contains illegalValueChar str) then
-        Just (detailedError str)
+        Just (valueError str)
     else
         Nothing
